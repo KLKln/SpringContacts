3144776a43d5b81246ca34e3d09b30ab226b4851
Kelly Klein <kmklein2@dmacc.edu>
2020-03-23 17:54:36 -0500

    Working Program

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,14 +19,8 @@ import dmacc.repository.ContactRepository;
 public class WebController {
 	@Autowired
 	ContactRepository repo;
-	
-	@GetMapping("/viewAll")
-		public String viewAllContacts(Model model) {
-		model.addAttribute("contacts", repo.findAll());
-		return "results";
-		}
-		/*
-		GetMapping("viewAll")
+
+	@GetMapping({ "/", "viewAll" })
 	public String viewAllContacts(Model model) {
 		if(repo.findAll().isEmpty()) {
 			return addNewContact(model);
@@ -34,43 +28,34 @@ public class WebController {
 		
 		model.addAttribute("contacts", repo.findAll());
 		return "results";
-	}*/
-	
-	/*@GetMapping({ "/", "viewAll" })
-		public String viewAllContacts(Model model) {
-		if(repo.findAll().isEmpty()) {
-			return addNewContact(model);
-		}
-		model.addAttribute("contacts", repo.findAll());
-		return "results";
-		}*/
-	
+	}
+
 	@GetMapping("/inputContact")
-	public String addNewContact(@ModelAttribute Contact c, Model model) {
+	public String addNewContact(Model model) {
+		Contact c = new Contact();
+		model.addAttribute("newContact", c);
+		return "input";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String showUpdateContact(@PathVariable("id") long id, Model model) {
+		Contact c = repo.findById(id).orElse(null);
+		System.out.println("ITEM TO EDIT: " + c.toString());
+		model.addAttribute("newContact", c);
+		return "input";
+	}
+
+	@PostMapping("/update/{id}")
+	public String reviseContact(Contact c, Model model) {
 		repo.save(c);
 		return viewAllContacts(model);
 	}
 	
-	@GetMapping("/edit/{id}")
-	public String showUpdateContact(@PathVariable("id") long id,
-	Model model) {
-	Contact c = repo.findById(id).orElse(null);
-	model.addAttribute("newContact", c);
-	return "input";
-	}
-	
-	@PostMapping("/update/{id}")
-	public String reviseContact(Contact c, Model model) {
-	repo.save(c);
-	return viewAllContacts(model);
-	}
-	
 	@GetMapping("/delete/{id}")
-	public String deleteUser(@PathVariable("id") long id, Model
-	model) {
-	Contact c = repo.findById(id).orElse(null);
-	 repo.delete(c);
-	 return viewAllContacts(model);	
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+		Contact c = repo.findById(id).orElse(null);
+	    repo.delete(c);
+	    return viewAllContacts(model);
 	}
 	
 	
